b173be3ae892dd30dd027d1132b6a32406bdc35f
Enoth <58894013+nikitairl@users.noreply.github.com>
2024-03-18 15:22:34 +0300

    ADD TrialPeriod

--- pay2u/subscriptions/admin.py
+++ pay2u/subscriptions/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from .models import Subscription
+from .models import Subscription, TrialPeriod
 
 
 @admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
     list_display = ("name", "availability", "price", "period", "cashback")
+    pass
+
+
+@admin.register(TrialPeriod)
+class TrialPeriodAdmin(admin.ModelAdmin):
+    list_display = ("period_days", "period_cost")
     pass
--- pay2u/subscriptions/models.py
+++ pay2u/subscriptions/models.py
@@ -3,18 +3,37 @@ from django.db import models
 from .constants import SUB_METHODS
 
 
+class TrialPeriod(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    period_days = models.IntegerField('Срок пробного периода', default=0)
+    period_cost = models.IntegerField('Стоимость пробного периода', default=0)
+
+    class Meta:
+        verbose_name = "Пробная подписка"
+        verbose_name_plural = "Пробные подписки"
+
+    def __str__(self):
+        return str(f'{self.id} - {self.period_days}, {self.period_cost}р.')
+
+
 class Subscription(models.Model):
     id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     availability = models.BooleanField()
     price = models.IntegerField()
     period = models.IntegerField()
     cashback = models.IntegerField()
     service_id = models.ForeignKey(
         "services.Service",
-        on_delete=models.CASCADE
+        on_delete=models.RESTRICT,
+        related_name="subscription",
     )
-    trial_period = models.IntegerField()
+    trial_period = models.ForeignKey(
+        TrialPeriod,
+        on_delete=models.RESTRICT,
+        blank=True,
+        null=True,
+    )
     activation_method = models.CharField(max_length=20, choices=SUB_METHODS)
 
     class Meta:
